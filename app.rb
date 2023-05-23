72e6b4506ffec0ea1078c5ee28be6bb99d17e31e
GeralmyUrena <geralmyuri11@gmail.com>
2023-05-23 19:07:07 -0400

    commit tree 800e8c76237e39616179cb17903577a348c6ee7e

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
-# p [1,2,3]
-# puts "Hello World!"
+
 # print "Pass this test, please."
 # p [1,2,3]
 
@@ -14,10 +12,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
